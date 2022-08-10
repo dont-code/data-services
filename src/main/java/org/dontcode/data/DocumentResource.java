7a81e95a6075b68eafcf897b8ea495b612d2b369
Gérard Collin <gerard.collin@appliweb.net>
2022-08-10 21:28:37 +0200

    Ensure the dest dir is created

--- src/main/java/org/dontcode/data/DocumentResource.java
+++ src/main/java/org/dontcode/data/DocumentResource.java
@@ -46,8 +46,9 @@ public class DocumentResource {
             for (FileUpload file : filesForm.files) {
                 filePath=file.uploadedFile().getFileName().toString()+'.'+file.contentType().substring(file.contentType().lastIndexOf('/')+1);
                 java.nio.file.Path destPath = documentDir.resolve(filePath);
-                log.info("Upload file is {}, exist ? {}, Dest path is {}", file.uploadedFile(), Files.exists(file.uploadedFile()), destPath);
-                Files.copy(file.uploadedFile().toAbsolutePath(), destPath, StandardCopyOption.REPLACE_EXISTING);
+                log.info("Uploaded file is {}, exist ? {}, Dest path is {}", file.uploadedFile(), Files.exists(file.uploadedFile()), destPath);
+                Files.copy(file.uploadedFile(), documentDir, StandardCopyOption.REPLACE_EXISTING);
+                Files.move(documentDir.resolve(file.uploadedFile().getFileName()), destPath);
                 Files.setPosixFilePermissions(destPath, PosixFilePermissions.fromString("rwxr-xr-x"));
                 resp.add(new UploadedDocumentInfo(file.fileName(), true, docExternalUrl+'/'+ URLEncoder.encode( filePath, Charset.defaultCharset())));
                 log.debug ("Received document {} to url {}", file.fileName(), docExternalUrl+'/'+ URLEncoder.encode( filePath, Charset.defaultCharset()));
